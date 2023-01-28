07d033322a4c8bf6b2c40221471b489fba411f5a
rveranervi <rveranervi@gmail.com>
2023-01-28 04:01:54 -0500

    Thymeleaf

--- src/main/java/pe/sermed/backend/controller/thymeleaf/HomeController.java
+++ src/main/java/pe/sermed/backend/controller/thymeleaf/HomeController.java
@@ -1,17 +1,38 @@
 package pe.sermed.backend.controller.thymeleaf;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pe.sermed.backend.model.dto.DoctorDTO;
+import pe.sermed.backend.service.AppointmentService;
+import pe.sermed.backend.service.DoctorService;
+import pe.sermed.backend.service.PatientService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
-    @GetMapping
-    public ModelAndView mostrarIndex() {
-        ModelAndView mv = new ModelAndView("index");
-        //mv.addObject("listaCategorias", categoriaService.listarTodos());
-        return mv;
+
+    private DoctorService doctorService;
+    private PatientService patientService;
+    private AppointmentService appointmentService;
+
+    public HomeController(DoctorService doctorService,
+                          PatientService patientService,
+                          AppointmentService appointmentService){
+        this.doctorService = doctorService;
+        this.patientService = patientService;
+        this.appointmentService = appointmentService;
+    }
+
+    @GetMapping("/")
+    public String showAll(Model model) {
+        model.addAttribute("doctors", doctorService.listAll(-1));
+        model.addAttribute("patients", patientService.listAll());
+        model.addAttribute("appointments", appointmentService.listAll());
+        return "index";
     }
 }
